9a506d13fbe3399384d57c70be6ede225871a4ba
set0xc3 <set0xc3@gmail.com>
2023-10-04 11:03:13 +0300

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,22 @@ main(int, char **)
   os_init(false);
   gfx_init();
 
+  DB_pg_connect("host=172.17.0.2 port=5432 dbname=dev user=postgres "
+                "password=root connect_timeout=10");
+
+  if (!DB_pg_connect_is_valid()) {
+    DB_pg_finish();
+    return 1;
+  }
+
+  PGresult *pg_result = DB_pg_exec("SELECT * FROM clients");
+  if (!pg_result) {
+    DB_pg_finish();
+  }
+
+  u32 pg_result_rows = PQntuples(pg_result);
+  u32 pg_result_cols = PQnfields(pg_result);
+
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   ImGuiIO &io = ImGui::GetIO();
@@ -18,6 +34,10 @@ main(int, char **)
       |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
   io.ConfigFlags
       |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+
+  io.Fonts->AddFontFromFileTTF(
+      "/usr/share/fonts/adobe-source-code-pro-fonts/SourceCodePro-Medium.otf",
+      18.f, nullptr, ImGui::GetIO().Fonts->GetGlyphRangesCyrillic());
 
   ImGui::StyleColorsLight();
 
@@ -48,6 +68,74 @@ main(int, char **)
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
     {
+      static bool             use_work_area = true;
+      static ImGuiWindowFlags flags         = ImGuiWindowFlags_NoDecoration
+                                      | ImGuiWindowFlags_NoMove
+                                      | ImGuiWindowFlags_NoSavedSettings;
+
+      const ImGuiViewport *viewport = ImGui::GetMainViewport();
+      ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos
+                                            : viewport->Pos);
+      ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize
+                                             : viewport->Size);
+
+      if (ImGui::Begin("RootWindow", NULL, flags)) {
+        ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None;
+        if (ImGui::BeginTabBar("MyTabBar", tab_bar_flags)) {
+          if (ImGui::BeginTabItem("Ремонты")) {
+            ImGui::Text("Ремонты");
+            ImGui::EndTabItem();
+          }
+          if (ImGui::BeginTabItem("Товары")) {
+            ImGui::Text("Товары");
+            ImGui::EndTabItem();
+          }
+          if (ImGui::BeginTabItem("Клиенты")) {
+            ImGui::Text("Клиенты");
+
+            static ImGuiTableFlags flags
+                = ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg;
+            static bool display_headers = false;
+
+            if (ImGui::BeginTable("table_clients", 6, flags)) {
+              ImGui::TableSetupColumn("ID");
+              ImGui::TableSetupColumn("Ф.И.О");
+              ImGui::TableSetupColumn("Телефон");
+              ImGui::TableSetupColumn("Адрес");
+              ImGui::TableSetupColumn("Эл.почта");
+              ImGui::TableSetupColumn("Дата регистрации");
+              ImGui::TableHeadersRow();
+
+              for (u32 row = 0; row < pg_result_rows; row++) {
+                ImGui::TableNextRow();
+                for (u32 i = 0; i < 6; i++) {
+                  ImGui::TableSetColumnIndex(i);
+                  ImGui::TextUnformatted(PQgetvalue(pg_result, row, i + 1));
+                }
+                ImGui::Button("X");
+              }
+            }
+            ImGui::EndTable();
+            ImGui::EndTabItem();
+          }
+          if (ImGui::BeginTabItem("Финансы")) {
+            ImGui::Text("Финансы");
+            ImGui::EndTabItem();
+          }
+          if (ImGui::BeginTabItem("Отчёты")) {
+            ImGui::Text("Отчёты");
+            ImGui::EndTabItem();
+          }
+          if (ImGui::BeginTabItem("Задания")) {
+            ImGui::Text("Задания");
+            ImGui::EndTabItem();
+          }
+          ImGui::EndTabBar();
+        }
+
+        ImGui::ShowDemoWindow();
+      }
+      ImGui::End();
     }
     ImGui::Render();
 
@@ -58,6 +146,8 @@ main(int, char **)
 
     ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
     os_window_swap_buffer(os_window_root_get());
+
+    os_delay(1);
   }
 
   // Cleanup
