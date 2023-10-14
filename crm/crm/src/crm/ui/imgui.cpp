5ad73d81adcb3ac770d3e0e42a6071830fc495a6
set0xc3 <set0xc3@gmail.com>
2023-10-14 15:28:48 +0300

    Update and fix imgui

--- crm/crm/src/crm/ui/imgui.cpp
+++ crm/crm/src/crm/ui/imgui.cpp
@@ -12,15 +12,21 @@ imgui_init(void)
   ImGuiIO &io = ImGui::GetIO();
   (void)io;
   io.ConfigFlags
-      |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-  io.ConfigFlags
-      |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+      |= ImGuiConfigFlags_NavEnableKeyboard;        // Enable Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Docking
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport
 
   io.Fonts->AddFontFromFileTTF("assets/fonts/SourceCodePro-Medium.otf", 18.0f,
                                nullptr,
                                ImGui::GetIO().Fonts->GetGlyphRangesCyrillic());
 
   ImGui::StyleColorsLight();
+
+  ImGuiStyle &style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    style.WindowRounding              = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
 
   ImGui_ImplSDL2_InitForOpenGL(os_window_root_get()->sdl.window,
                                os_window_root_get()->sdl.gl_ctx);
@@ -41,4 +47,14 @@ void
 imgui_render(void)
 {
   ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    SDL_Window   *backup_current_window  = SDL_GL_GetCurrentWindow();
+    SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+    ImGui::UpdatePlatformWindows();
+    ImGui::RenderPlatformWindowsDefault();
+    SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+  }
 }
